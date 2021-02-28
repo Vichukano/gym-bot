387e546bc9dd2813fe8aaa28711d257092136aaf
Vichukano <vichukano@gmail.com>
2021-02-28 18:50:01 +0300

    add google drive

--- gym-bot-app/src/main/java/ru/vichukano/gym/bot/Main.java
+++ gym-bot-app/src/main/java/ru/vichukano/gym/bot/Main.java
@@ -1,9 +1,22 @@
 package ru.vichukano.gym.bot;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.vichukano.gym.bot.dao.google.disk.UserGoogleDiskDao;
 import ru.vichukano.gym.bot.factory.HandlerFactory;
 import ru.vichukano.gym.bot.factory.StateToHandlerFactory;
 import ru.vichukano.gym.bot.handler.document.SendReportUpdateHandler;
@@ -12,16 +25,32 @@ import ru.vichukano.gym.bot.service.UserService;
 import ru.vichukano.gym.bot.telegram.GymBot;
 import ru.vichukano.gym.bot.util.PropertiesReader;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
 import java.util.Properties;
+import java.util.Set;
 
 @Slf4j
 public class Main {
+    private static final String APPLICATION_NAME = "Gym Bot";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final Set<String> SCOPES = DriveScopes.all();
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException, GeneralSecurityException, IOException {
         log.debug("Starting bot!");
         Properties props = PropertiesReader.load("app.properties");
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive driveService = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
         var service = new UserService(
-                new UserExcelDao(
+                new UserGoogleDiskDao(
+                        driveService,
                         props.getProperty("store").isEmpty() ? System.getProperty("java.io.tmpdir") : props.getProperty("store")
                 )
         );
@@ -38,6 +67,21 @@ public class Main {
                                 props.getProperty("token")
                         )
                 );
+    }
+
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = Main.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("Vichukano");
     }
 
 }
---
+++ gym-bot-dao-google-disk/src/main/java/ru/vichukano/gym/bot/dao/google/disk/UserGoogleDiskDao.java
@@ -0,0 +1,142 @@
+package ru.vichukano.gym.bot.dao.google.disk;
+
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.Drive;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import ru.vichukano.gym.bot.UserDao;
+import ru.vichukano.gym.bot.model.Exercise;
+import ru.vichukano.gym.bot.model.SavedUser;
+import ru.vichukano.gym.bot.model.Training;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Stream;
+
+@Slf4j
+@RequiredArgsConstructor
+public class UserGoogleDiskDao implements UserDao {
+    private static final String FILE_TYPE = ".xlsx";
+    private static final String NEW = "new";
+    private final Drive driveService;
+    private final String path;
+
+    @SneakyThrows
+    @Override
+    public void saveOrUpdate(SavedUser user) {
+        log.trace("Start to save for: {}", user);
+        Optional<File> file = getByFileName(user.getId() + user.getName());
+        XSSFWorkbook workbook;
+        if (file.isPresent()) {
+            workbook = (XSSFWorkbook) WorkbookFactory.create(file.get());
+        } else {
+            workbook = new XSSFWorkbook();
+        }
+        Sheet sheet = workbook.createSheet("Training " + (workbook.getNumberOfSheets() + 1) + " " + lastTrainDate(user.getTrainings()));
+        sheet.setColumnWidth(0, 6000);
+        sheet.setColumnWidth(1, 6000);
+        sheet.setColumnWidth(2, 6000);
+        Row header = sheet.createRow(0);
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        XSSFFont font = workbook.createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 14);
+        font.setBold(true);
+        headerStyle.setFont(font);
+        Cell exerciseCell = header.createCell(0);
+        exerciseCell.setCellValue("Exercise");
+        exerciseCell.setCellStyle(headerStyle);
+        Cell weightCell = header.createCell(1);
+        weightCell.setCellValue("Weight");
+        weightCell.setCellStyle(headerStyle);
+        Cell repsCell = header.createCell(2);
+        repsCell.setCellValue("Reps");
+        repsCell.setCellStyle(headerStyle);
+        List<Exercise> exercises = fromLastTraining(user.getTrainings());
+        int position = 1;
+        for (Exercise exercise : exercises) {
+            List<BigDecimal> weights = exercise.getWeights();
+            for (int j = 0; j < weights.size(); j++) {
+                Row nextExerciseRow = sheet.createRow(position);
+                Cell name = nextExerciseRow.createCell(0);
+                name.setCellValue(exercise.getName());
+                makeBold(name, workbook);
+                Cell weight = nextExerciseRow.createCell(1);
+                weight.setCellValue(weights.get(j).doubleValue());
+                Cell reps = nextExerciseRow.createCell(2);
+                reps.setCellValue(exercise.getReps().get(j));
+                position++;
+            }
+        }
+        String fileName = path + user.getId() + user.getName() + FILE_TYPE;
+        try (var out = new FileOutputStream(fileName + NEW)) {
+            workbook.write(out);
+        } catch (IOException e) {
+            log.error("Exception while saving for: {}", user, e);
+        }
+        java.nio.file.Files.deleteIfExists(Paths.get(fileName));
+        Files.move(Paths.get(fileName + NEW), Paths.get(fileName));
+        log.trace("Finish saving for: {}", user);
+        com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+        fileMetadata.setName(user.getId() + user.getName() + FILE_TYPE);
+        fileMetadata.setMimeType("application/vnd.google-apps.spreadsheet");
+        FileContent mediaContent = new FileContent("text/xlsx", new File(fileName));
+        com.google.api.services.drive.model.File toDrive = driveService.files().create(fileMetadata, mediaContent)
+                .setFields("id")
+                .execute();
+        log.debug("Successfully execute file to Google Drive: {}", toDrive);
+    }
+
+    @SneakyThrows
+    @Override
+    public Optional<File> getByFileName(String fileName) {
+        return Optional.empty();
+    }
+
+    private String lastTrainDate(Collection<Training> trainings) {
+        return Stream.ofNullable(trainings)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(Training::getTime)
+                .filter(Objects::nonNull)
+                .map(LocalDateTime::toLocalDate)
+                .filter(Objects::nonNull)
+                .map(Objects::toString)
+                .reduce((first, second) -> second)
+                .orElse(LocalDate.now().toString());
+    }
+
+    private List<Exercise> fromLastTraining(Collection<Training> trainings) {
+        return Stream.ofNullable(trainings)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .reduce((first, second) -> second)
+                .map(Training::getExercises)
+                .orElse(Collections.emptyList());
+    }
+
+    private void makeBold(Cell cell, final Workbook workbook) {
+        CellStyle bold = workbook.createCellStyle();
+        bold.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
+        bold.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        XSSFFont font = (XSSFFont) workbook.createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 12);
+        font.setBold(true);
+        bold.setFont(font);
+        cell.setCellStyle(bold);
+    }
+}
