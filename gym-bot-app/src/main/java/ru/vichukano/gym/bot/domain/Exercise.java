d5a3b486852d6cb30d755c9b5915d055ccacdefc
Виктор Чук <vichukano@LAPTOP-B36NOH3R.localdomain>
2022-06-21 00:16:52 +0300

    23 add new exercises

--- gym-bot-app/src/main/java/ru/vichukano/gym/bot/domain/Exercise.java
+++ gym-bot-app/src/main/java/ru/vichukano/gym/bot/domain/Exercise.java
@@ -17,7 +17,11 @@ public enum Exercise {
     ABS("/abs", "exercises for abs"),
     PULL_UP("/pull_up", "pull ups"),
     PUSH_UP("/push_up", "push ups"),
-    PUSH_UP_ON_BARS("/push_up_on_bars", "push ups on the uneven bars");
+    PUSH_UP_ON_BARS("/push_up_on_bars", "push ups on the uneven bars"),
+    DUMBBELLS_BICEPS_CURL("/dumbbells_biceps_curl", "dumbbells biceps curls"),
+    BARBELL_BECEPS_CURL("/barbell_biceps_curl", "burbell biceps curls"),
+    HACK_SQUAT_MACHINE("/squat_machine", "hack squat machine"),
+    LEG_PRESS_MACHINE("/leg_press_machine", "leq press machine");
 
     @Getter
     private final String command;
